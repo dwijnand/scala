69aeb0267bdcd3b30a58cc4fccb936b4e77a5ba4
Dale Wijnand <dale.wijnand@gmail.com>
2021-02-15 10:26:00 +0000

    wip

--- test/files/pos/t9657-less-fishy.scala
+++ test/files/pos/t9657-less-fishy.scala
@@ -16,6 +16,8 @@ case class Bus(fuel: Int) extends Vehicle { type A = Petrol.type }
 case class Car(fuel: Int) extends Vehicle { type A = Petrol.type }
 
 object Test {
+  // perhaps this is still fishy... why not just:
+//def refuel(vehicle: Vehicle { type A <: NonRenewablePowerSource }): Vehicle = ...
   def refuel[P <: NonRenewablePowerSource](vehicle: Vehicle { type A = P }): Vehicle = vehicle match {
     case Car(_) => Car(100)
     case Bus(_) => Bus(100) // was: "unreachable code" warning
